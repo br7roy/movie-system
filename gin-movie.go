f5d0e8c293694c1605fdea2e2f13c3891316a255
Tak <br7roy@gmail.com>
2020-03-02 17:10:51 +0800

    feat(initializing project):

---
+++ gin-movie.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+/**
+ref https://github.com/bddbnet/gin_readme_zh
+*/
+func main() {
+
+	engine := gin.Default()
+	engine.LoadHTMLGlob("templates/*")
+	engine.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"hello": "https://takfu.tk/movie",
+			"blog":  "https://takblog.netlify.com",
+		})
+	})
+	engine.GET("/index", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Movie coming.",
+			"text":  "Hello Gin",
+		})
+	})
+	engine.Run(":8080")
+
+}
