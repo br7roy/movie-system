8e6da85c9cd7d1f95df81c2f51190fe2f8afe2c5
Tak <br7roy@gmail.com>
2020-03-04 17:29:25 +0800

    feat(页面跳转): 图标

--- movie.go
+++ movie.go
@@ -8,6 +8,7 @@ import (
 //crud https://blog.csdn.net/wangshubo1989/article/details/70313667
 //模板 https://www.popmars.com/category/html%e6%a8%a1%e6%9d%bf/
 // https://juejin.im/user/582c029e2e958a0069b1feb2/posts
+//图标 https://www.easyicon.net/1225507-camcorder_camera_movie_record_video_icon.html
 func main() {
 
 	engine := gin.Default()
