c1d2f2f54d4516a26f2cfa33eb175a6c1134fd49
Tak <br7roy@gmail.com>
2020-03-06 10:29:29 +0800

    update script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+echo $0 
+pdir=`cd -P $(dirname $0);pwd` 
+echo $pdir
+starttime=`date +'%Y-%m-%d %H:%M:%S'` 
+echo building artifact start
+echo at:$starttime
+go build -o linux/movie ../ 
+endtime=`date +'%Y-%m-%d %H:%M:%S'` 
+start_seconds=$(date --date="$starttime" +%s)
+end_seconds=$(date --date="$endtime" +%s)
+echo done
+echo Finish at:$endtime
+echo "elapse: "$((end_seconds-start_seconds))"s"
 
-go build -o  linux/movie ../
