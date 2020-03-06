f42ab4cd65b3e84592d5c09ee4377c41034eb517
Tak <br7roy@gmail.com>
2020-03-06 10:58:50 +0800

    update script

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo $0 
-pdir=`cd -P $(dirname $0);pwd` 
-echo $pdir
+dir=`cd -P $(dirname $0);pwd` 
+pdir=${dir%/*}
+echo rootDir is $pdir
 starttime=`date +'%Y-%m-%d %H:%M:%S'` 
 echo building artifact start
 echo at:$starttime
-go build -o linux/movie ../ 
+go build -o $pdir/bin/movie $pdir
 endtime=`date +'%Y-%m-%d %H:%M:%S'` 
 start_seconds=$(date --date="$starttime" +%s)
 end_seconds=$(date --date="$endtime" +%s)
