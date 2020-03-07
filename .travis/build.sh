f236b2d7bdac863d388ce55f95349388f7569c19
Tak <br7roy@gmail.com>
2020-03-07 12:06:27 +0800

    fix: script comment

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,14 +2,15 @@
 
 git remote set-url origin git@github.com:br7roy/movie-system.git
 git remote -v
+git clone --branch origin master git@github.com:br7roy/movie-system.git ~/.master_deploy
 
 dir=`cd -P $(dirname $0);pwd` 
 pdir=${dir%/*}
 echo rootDir is $pdir
 rm -fr $pdir/bin
 starttime=`date +'%Y-%m-%d %H:%M:%S'`
-echo building artifact start
-echo at:$starttime
+echo building artifact...
+echo start at:$starttime
 go build -o $pdir/bin/movie $pdir
 endtime=`date +'%Y-%m-%d %H:%M:%S'` 
 start_seconds=$(date --date="$starttime" +%s)
@@ -18,7 +19,9 @@ echo building done
 echo Finish at:$endtime
 echo "elapse: "$((end_seconds-start_seconds))"s"
 
-cd $pdir
+mv -fv $pdir/** ~/.master_deploy
+cd ~/.master_deploy
+
 git add -A
 
 ls -l $pdir
