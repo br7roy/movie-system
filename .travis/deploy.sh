bb4aa45dce9c3bd71b6f49982c315519c1a8a69f
Tak <br7roy@gmail.com>
2020-03-07 13:04:30 +0800

    fix: script comment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 masterDep() {
 
   dir=$(
@@ -8,13 +9,8 @@ masterDep() {
   pdir=${dir%/*}
   echo rootDir is $pdir
   rm -fr $pdir/bin
-  starttime=$(date +'%Y-%m-%d %H:%M:%S')
-  echo -e "building artifact...\nstart at:${starttime}"
+
   go build -o $pdir/bin/movie $pdir
-  endtime=$(date +'%Y-%m-%d %H:%M:%S')
-  start_seconds=$(date --date="$starttime" +%s)
-  end_seconds=$(date --date="$endtime" +%s)
-  echo -e "building done\nFinish at:${endtime}\nelapse: $((end_seconds - start_seconds))s"
 
   mv -fv $pdir/* ~/.master_deploy
   cd ~/.master_deploy
@@ -29,11 +25,17 @@ masterDep() {
 }
 
 goTest() {
-  echo starting test.
+
   go test -v $(pwd)
-  echo starting test done.
+
 }
 
+starttime=$(date +'%Y-%m-%d %H:%M:%S')
+echo -e "start go test...\nstart at:${starttime}"
 goTest
+echo -e "go test done.\n start deploy master."
 masterDep
-echo all done .
+endtime=$(date +'%Y-%m-%d %H:%M:%S')
+start_seconds=$(date --date="$starttime" +%s)
+end_seconds=$(date --date="$endtime" +%s)
+echo -e "all done\nFinish at:${endtime}\nelapse: $((end_seconds - start_seconds))s"
